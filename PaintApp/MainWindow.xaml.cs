fee4328bbe3b3e7d94b666c8a95162e1ffc8080f
NguyenVH <nvo010204@gmail.com>
2025-12-05 22:21:32 +0700

    MainWindow layout using NavigationView

--- PaintApp/MainWindow.xaml.cs
+++ PaintApp/MainWindow.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -12,9 +14,7 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using Windows.Storage;
 
 namespace PaintApp
 {
@@ -23,9 +23,94 @@ namespace PaintApp
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        private readonly ApplicationDataContainer _localSettings;
+
         public MainWindow()
         {
             InitializeComponent();
+            _localSettings = ApplicationData.Current.LocalSettings;
+            
+            SetupCustomTitleBar();
+            LoadTheme();
+            
+            NavView.SelectedItem = NavView.MenuItems[0];
+        }
+
+        private void SetupCustomTitleBar()
+        {
+            ExtendsContentIntoTitleBar = true;
+            SetTitleBar(AppTitleBar);
+            
+            if (AppWindowTitleBar.IsCustomizationSupported())
+            {
+                var titleBar = AppWindow.TitleBar;
+                titleBar.ExtendsContentIntoTitleBar = true;
+                titleBar.ButtonBackgroundColor = Colors.Transparent;
+                titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+            }
+        }
+
+        private void LoadTheme()
+        {
+            if (_localSettings.Values["AppTheme"] is string themeName)
+            {
+                if (Content is FrameworkElement rootElement)
+                {
+                    rootElement.RequestedTheme = themeName switch
+                    {
+                        "Light" => ElementTheme.Light,
+                        "Dark" => ElementTheme.Dark,
+                        _ => ElementTheme.Default
+                    };
+                }
+            }
+        }
+
+        private void ThemeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Content is FrameworkElement rootElement)
+            {
+                var currentTheme = rootElement.ActualTheme;
+                var newTheme = currentTheme == ElementTheme.Light ? ElementTheme.Dark : ElementTheme.Light;
+                rootElement.RequestedTheme = newTheme;
+                
+                _localSettings.Values["AppTheme"] = newTheme.ToString();
+            }
+        }
+
+        private void NavView_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+        {
+            if (ContentFrame.CanGoBack)
+            {
+                ContentFrame.GoBack();
+            }
+        }
+
+        private void ContentFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+            NavView.IsBackEnabled = ContentFrame.CanGoBack;
+        }
+
+        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.IsSettingsSelected)
+            {
+                ContentFrame.Navigate(typeof(Page));
+            }
+            else if (args.SelectedItemContainer != null)
+            {
+                string tag = args.SelectedItemContainer.Tag?.ToString() ?? string.Empty;
+                
+                switch (tag)
+                {
+                    case "Home":
+                        break;
+                    case "Canvas":
+                        break;
+                    case "Profiles":
+                        break;
+                }
+            }
         }
     }
 }
